fe9a5bae5262ca4253c6b5576f92e1c79e8c304d
Christopher Birkenhagen <c.birkenhagen@gmail.com>
2020-05-13 00:04:26 -0400

    Feature add:  grid set. mouse drag. animated game piece. click to add game piece.

---
+++ main.js
@@ -0,0 +1,114 @@
+const boardTiles = document.querySelectorAll('.box')
+
+let dupType = 'biter'
+const pickers = document.querySelectorAll('.picker')
+pickers.forEach(pic => {
+    pic.addEventListener('click', e => dupType = e.target.dataset.type)
+})
+
+boardTiles.forEach(box => {
+    box.addEventListener('click', e => {
+        let div = document.createElement('div')
+        div.classList.add(`${dupType}`,'animation','item')
+        // div.classList.add('biter','animation','item')
+        div.addEventListener('mousedown', setDrag)
+        div.addEventListener('touchstart', setDrag)
+        div.addEventListener('click', e => {
+            e.stopPropagation()
+        })
+        e.target.appendChild(div)
+    })
+})
+
+function setDrag(e) {
+    e.stopPropagation()
+    // console.log('mousedown')
+    animateScript(e.target)
+    console.log(dragItem)
+    dragItem = e.target
+}
+
+function animateScript(obj) {
+    let imgSize = 77
+    let position = imgSize;
+    const interval = 100;
+    setInterval(() => {
+    obj.style.backgroundPosition = `-${position}px 0px`;
+    if (position < 480) position += imgSize;
+    else position = imgSize;
+  }, interval);
+}
+
+//  Mouse Drag Tutorial: https://www.kirupa.com/html5/drag.htm
+
+let dragItem = document.querySelector(".item");
+let board = document.querySelector(".board");
+
+let active = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+let dragEndTarget;
+
+board.addEventListener("touchstart", dragStart, false);
+board.addEventListener("touchend", dragEnd, false);
+board.addEventListener("touchmove", drag, false);
+
+board.addEventListener("mousedown", dragStart, false);
+board.addEventListener("mouseup", dragEnd, false);
+board.addEventListener("mousemove", drag, false);
+
+boardTiles.forEach(box => {
+    box.addEventListener('touchend', e => {
+        dragEndTarget = e.target.dataset.index;
+        console.log(`Ending in box: ${e.target.dataset.index}`)
+    })
+})
+
+function dragStart(e) {
+    if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+    } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+    }
+
+    if (e.target === dragItem) {
+    active = true;
+    }
+}
+
+function dragEnd(e) {
+    initialX = currentX;
+    initialY = currentY;
+
+    active = false;
+}
+
+function drag(e) {
+    if (active) {
+    
+    e.preventDefault();
+    
+    if (e.type === "touchmove") {
+        currentX = e.touches[0].clientX - initialX;
+        currentY = e.touches[0].clientY - initialY;
+    } else {
+        currentX = e.clientX - initialX;
+        currentY = e.clientY - initialY;
+    }
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, dragItem);
+    }
+}
+
+function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
